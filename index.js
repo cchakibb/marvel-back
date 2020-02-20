2b97174e548a6f5c0616d2f3af2928e5fdd648ce
Chakib B <57419800+chakhub@users.noreply.github.com>
2020-02-20 18:56:04 +0100

    ready for hosting

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
 const express = require("express");
-const formidableMiddleware = require("express-formidable");
 const server = express();
+const formidableMiddleware = require("express-formidable");
 server.use(formidableMiddleware());
 
-server.get("/", (req, res) => {
-  res.json({ message: "Hi" });
+const axios = require("axios");
+const cors = require("cors");
+server.use(cors());
+require("dotenv").config();
+const uid2 = require("uid2");
+const md5 = require("md5");
+
+const publicKey = process.env.PUBLIC_KEY;
+const privateKey = process.env.PRIVE_KEY;
+const ts = uid2(10);
+const hash = md5(ts + privateKey + publicKey);
+console.log("ts = ", ts);
+console.log("hash = ", hash);
+
+server.get("/", async (req, res) => {
+  const response = await axios.get(
+    `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=${ts}&apikey=${publicKey}&hash=${hash}`
+  );
+  console.log(response.data);
+
+  res.json(response.data);
+});
+
+server.get("/characters", async (req, res) => {
+  const response = await axios.get(
+    `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=${ts}&apikey=${publicKey}&hash=${hash}`
+  );
+  console.log(response.data);
+
+  res.json(response.data);
+});
+
+server.get("/comics", async (req, res) => {
+  const response = await axios.get(
+    `https://gateway.marvel.com:443/v1/public/comics?orderBy=title&limit=100&ts=${ts}&apikey=${publicKey}&hash=${hash}`
+  );
+  console.log(response.data);
+  res.json(response.data);
+});
+
+server.get("/comic/:id", async (req, res) => {
+  const id = req.params.id;
+  const response = await axios.get(
+    `https://gateway.marvel.com:443/v1/public/characters/${id}/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+  );
+  console.log(response.data);
+  res.json(response.data);
 });
 
 server.all("*", function(req, res) {
   res.json({ message: "all routes" });
 });
 
-server.listen(3100, () => {
+server.listen(process.env.PORT, () => {
   console.log("Server has started");
 });
